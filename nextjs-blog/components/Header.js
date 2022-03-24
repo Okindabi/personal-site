1ce7011fd9f14180964f222aa0f0b8078dcdbab9
Nicholas Badiola <NicholasBadiola@gmail.com>
2022-03-24 17:24:54 -0400

    started setting up tailwindcss

---
+++ nextjs-blog/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+
+return (
+    <div>
+        <h1 className="text-3xl font-bold underline">Hello,</h1>
+        <p>I am a full-stack web developer. </p>
+        <p>Niko Badiola</p>
+        <p>NicholasBadiola@gmail.com</p>
+    </div>
+)
+}
+
+export default Header
